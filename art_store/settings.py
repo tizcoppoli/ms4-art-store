1c9c97b5f5af2b0b46f4a665d2d03dc54fccbf7c
tizianocoppoli <tiziano.coppoli@gmail.com>
2021-06-21 14:55:46 +0000

    deploy staticfiles to aws

--- art_store/settings.py
+++ art_store/settings.py
@@ -181,7 +181,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'ci-boutique-ado-store'
+    AWS_STORAGE_BUCKET_NAME = 'ms4-art-store'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
