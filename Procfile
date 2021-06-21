2d346694f39a2de79684db2370322f0b0591c615
tizianocoppoli <tiziano.coppoli@gmail.com>
2021-06-21 13:50:39 +0000

    deploy to heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn boutique_ado.wsgi:application
+web: gunicorn art_store.wsgi:application
--- art_store/settings.py
+++ art_store/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['ci-boutique-ado-store.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['ms4-art-store.herokuapp.com', 'localhost']
 
 
 # Application definition
